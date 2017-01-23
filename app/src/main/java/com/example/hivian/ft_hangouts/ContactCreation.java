cde91e9b6b339eae5a835f751344c314dd57034a
Hugues IVIAN <hivian@e2r6p11.42.fr>
2017-01-23 17:06:56 +0100

    up

--- app/src/main/java/com/example/hivian/ft_hangouts/ContactCreation.java
+++ app/src/main/java/com/example/hivian/ft_hangouts/ContactCreation.java
@@ -1,22 +1,31 @@
 package com.example.hivian.ft_hangouts;
 
+import android.Manifest;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.text.Html;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class ContactCreation extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_creation);
 
-        //Intent intent = getIntent();
         Button button = (Button)findViewById(R.id.button_save);
 
         getSupportActionBar().setTitle(Html.fromHtml("<font color='white'>" + getString(R.string.create_contact)  + "</font>"));
@@ -30,5 +39,33 @@ public class ContactCreation extends AppCompatActivity {
             button.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
             Log.d("DEBUG", "IS_BLUE");
         }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+    }
+
+    public void browseFolder(View view) {
+        Log.d("DEBUG", "BROWSE");
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(intent, RESULT_LOAD_IMAGE);
     }
 }
