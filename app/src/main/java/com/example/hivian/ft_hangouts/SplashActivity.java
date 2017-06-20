46e23a3f8be580160f6ab92200fa1da51673f57d
Hugues IVIAN <hivian@e2r6p11.42.fr>
2017-06-20 12:03:29 +0200

    splash screen OK

---
+++ app/src/main/java/com/example/hivian/ft_hangouts/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.example.hivian.ft_hangouts;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by hivian on 6/20/17.
+ */
+
+public class SplashActivity extends Activity {
+    private static final long DELAY = 1000;
+    private boolean scheduled = false;
+    private Timer splashTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        splashTimer = new Timer();
+        splashTimer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                SplashActivity.this.finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, DELAY);
+        scheduled = true;
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        if (scheduled)
+            splashTimer.cancel();
+        splashTimer.purge();
+    }
+}
